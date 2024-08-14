d6452c1ae58a0cb4197a2d6dfb8b32114206dfb4
Pierre Wieser <p.wieser@trychlos.org>
2024-08-14 11:56:22 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:tabbed',
-    version: '1.2.2',
+    version: '1.2.3-rc',
     summary: 'A Blaze component which provides a consistent tabbed display',
     git: 'https://github.com/trychlos/pwix-tabbed.git',
     documentation: 'README.md'
